ceba4fa628674b787e7fdb4c9bb1f8ec361d37bc
Kashish Mittal <tiyakashish5@gmail.com>
2026-05-21 20:27:01 +0530

    final edited server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(sanitizeMiddleware);
 app.use(compression());
 
 // CORS
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 
 // Body parsing
 app.use(express.json());
